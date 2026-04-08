c2b7a3c746cc3b77c9bea8489683d2f0fdb48d5e
Aline Munezero <alinemunezero920@gmail.com>
2026-04-08 08:21:45 +0200

    Accessibility page

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,15 +2,24 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"StepsDefinitions","hook"},
-        plugin = {"pretty",
+        glue = {"features","StepsDefinitions", "hook"},
+        monochrome = true,
+        plugin = {
+                "pretty",
                 "json:target/cucumber/report.json",
-                "html:target/cucumber/report.html"}
+                "html:target/cucumber/report.html"
+        }
+
 )
+public class TestRunner extends AbstractTestNGCucumberTests {
 
-
-public class TestRunner  extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
